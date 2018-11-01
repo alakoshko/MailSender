44acd9e5f4bd35d1ef18eee86817cc9ec3e42cab
alakoshko <alakoshko@gmail.com>
2018-11-01 18:01:35 +0300

    * файлы второго пункта

---
+++ MailSender/Infrastructure/ItemsCollectionControl.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSender.Infrastructure
+{
+    
+
+    /// <summary>
+    ///     
+    ///
+    /// </summary>
+    public class ItemsCollectionControl : Control
+    {
+
+        #region ItemSource : object - Элементы коллекции
+        /// <summary>Элементы коллекции</summary>
+        public static readonly DependencyProperty ItemSourceProperty =
+            DependencyProperty.Register(
+                nameof(ItemSource),
+                typeof(IEnumerable),
+                typeof(ItemsCollectionControl),
+                new PropertyMetadata(default(IEnumerable), OnItemSourcePropertyChanged));
+
+        private static void OnItemSourcePropertyChanged(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        {
+
+        }
+
+        /// <summary>Элементы коллекции</summary>
+        public IEnumerable ItemSource
+        {
+            get => (IEnumerable)GetValue(ItemSourceProperty);
+            set => SetValue(ItemSourceProperty, value);
+        }
+        #endregion
+
+        #region CreateCommand : ICommand - Команда создания объекта
+        /// <summary>Команда создания объекта</summary>
+        public static readonly DependencyProperty CreateCommandProperty =
+            DependencyProperty.Register(
+                nameof(CreateCommand),
+                typeof(ICommand),
+                typeof(ItemsCollectionControl),
+                new PropertyMetadata(default(ICommand)/*, OnCreateCommandPropertyChanged*/));
+
+        //private static void OnCreateCommandPropertyChanged(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        //{
+            
+        //}
+
+        /// <summary>Команда создания объекта</summary>
+        public ICommand CreateCommand
+        {
+            get => (ICommand)GetValue(CreateCommandProperty);
+            set => SetValue(CreateCommandProperty, value);
+        }
+        #endregion
+
+        #region DeleteCommand : ICommand - Команда удаления объекта
+        /// <summary>Команда удаления объекта</summary>
+        public static readonly DependencyProperty DeleteCommandProperty =
+            DependencyProperty.Register(
+                nameof(DeleteCommand),
+                typeof(ICommand),
+                typeof(ItemsCollectionControl),
+                new PropertyMetadata(default(ICommand)/*, OnDeleteCommandPropertyChanged*/));
+
+        //private static void OnDeleteCommandPropertyChanged(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        //{
+
+        //}
+
+        /// <summary>Команда удаления объекта</summary>
+        public ICommand DeleteCommand
+        {
+            get => (ICommand)GetValue(DeleteCommandProperty);
+            set => SetValue(DeleteCommandProperty, value);
+        }
+        #endregion
+
+        #region EditCommand : ICommand - Команда редактирования объекта
+        /// <summary>Команда редактирования объекта</summary>
+        public static readonly DependencyProperty EditCommandProperty =
+            DependencyProperty.Register(
+                nameof(EditCommand),
+                typeof(ICommand),
+                typeof(ItemsCollectionControl),
+                new PropertyMetadata(default(ICommand)/*, OnEditCommandPropertyChanged*/));
+
+        //private static void OnEditCommandPropertyChanged(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        //{
+
+        //}
+
+        /// <summary>Команда редактирования объекта</summary>
+        public ICommand EditCommand
+        {
+            get => (ICommand)GetValue(EditCommandProperty);
+            set => SetValue(EditCommandProperty, value);
+        }
+        #endregion
+
+        #region PanelName : string - Команда редактирования объекта
+        /// <summary>Команда редактирования объекта</summary>
+        public static readonly DependencyProperty PanelNameProperty =
+            DependencyProperty.Register(
+                nameof(PanelName),
+                typeof(string),
+                typeof(ItemsCollectionControl),
+                new PropertyMetadata(default(string)/*, OnPanelNamePropertyChanged*/));
+
+        //private static void OnPanelNamePropertyChanged(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        //{
+
+        //}
+
+        /// <summary>Команда редактирования объекта</summary>
+        public string PanelName
+        {
+            get => (string)GetValue(PanelNameProperty);
+            set => SetValue(PanelNameProperty, value);
+        }
+        #endregion
+
+        #region SelectedItem : object - Выбранный элемент
+        /// <summary>Команда редактирования объекта</summary>
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(
+                nameof(SelectedItem),
+                typeof(object),
+                typeof(ItemsCollectionControl),
+                new PropertyMetadata(default(object)/*, OnSelectedItemPropertyChanged*/));
+
+        //private static void OnSelectedItemPropertyChanged(DependencyObject D, DependencyPropertyChangedEventArgs E)
+        //{
+
+        //}
+
+        /// <summary>Команда редактирования объекта</summary>
+        public object SelectedItem
+        {
+            get => (object)GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+        #endregion
+        
+
+        static ItemsCollectionControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ItemsCollectionControl), new FrameworkPropertyMetadata(typeof(ItemsCollectionControl)));
+        }
+
+
+    }
+}
