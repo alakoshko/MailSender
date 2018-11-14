73291e87da26337e77c5b467970bfbc70428ec32
alakoshko <alakoshko@gmail.com>
2018-11-14 23:37:26 +0300

    * changed Mail Editor of MainWindow xaml

--- SpamLib/IDataAccessService.cs
+++ SpamLib/IDataAccessService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,14 @@ namespace SpamLib
     public interface IDataAccessService
     {
         ObservableCollection<Recipient> GetRecipients();
-        //Task<ObservableCollection<Recipient>> GetRecipientsAsync();
+        Task<ObservableCollection<Recipient>> GetRecipientsAsync();
 
         Guid CreateNewRecipientsDB(Recipient Recipients);
         void UpdateRecipientsDB(Recipient Recipients);
+
+        Task<ObservableCollection<ScheduledTask>> GetScheduledTasks();
+
+        Task<Guid> CreateRecipientsAsync(Recipient Recipients);
     }
 
     public class DataAccessServiceFromDB : IDataAccessService
@@ -26,12 +31,29 @@ namespace SpamLib
                 return new ObservableCollection<Recipient>(db.Recipients.ToArray());
         }
 
+        public async Task<ObservableCollection<Recipient>> GetRecipientsAsync()
+        {
+            using (var db = new SpamDB())
+                return new ObservableCollection<Recipient>(await db.Recipients.ToArrayAsync().ConfigureAwait(false));
+        }
+
         public Guid CreateNewRecipientsDB(Recipient recipient)
         {
             using (var db = new SpamDB())
             {
                 db.Recipients.Add(recipient);
                 if (db.SaveChanges() > 0)
+                    return recipient.Id;
+            }
+            return new Guid();
+        }
+
+        public async Task<Guid> CreateRecipientsAsync(Recipient recipient)
+        {
+            using (var db = new SpamDB())
+            {
+                db.Recipients.Add(recipient);
+                if (await db.SaveChangesAsync().ConfigureAwait(false) > 0)
                     return recipient.Id;
             }
             return new Guid();
@@ -46,6 +68,22 @@ namespace SpamLib
             catch(Exception e)
             {
                 throw new Exception(e.ToString());
+            }
+        }
+
+        public async Task<ObservableCollection<ScheduledTask>> GetScheduledTasks()
+        {
+            using (var db = new SpamDB())
+            {
+                return new ObservableCollection<ScheduledTask>(await db.ScheduledTasks
+                    .Include(task => task.Emails)
+                    .Include(task => task.MailingLists)
+                    .Include(task => task.MailingLists.Recepients)
+                    .Include(task => task.Senders)
+                    .Include(task => task.Servers)
+                    .ToArrayAsync()
+                    .ConfigureAwait(false)
+                    );
             }
         }
     }
